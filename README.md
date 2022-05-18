2b688b26dd4973f112dd25d603da2c1b036e9ff6
Kent Michael San <kentmichaelsan22@gmail.com>
2022-05-18 20:28:11 +0800

    Finish state transition

---
+++ README.md
@@ -0,0 +1,40 @@
+# Rock, Paper, Scissors, Lizard, Spock
+
+## The challenge
+
+Users should be able to:
+
+- View the optimal layout for the game depending on their device's screen size
+- Maintain the state of the score after refreshing the browser
+- Play Rock, Paper, Scissors, Lizard, Spock against the computer
+
+### Rules
+
+If the player wins, they gain 1 point. If the computer wins, the player loses one point.
+
+#### Original
+
+- Scissors beats Paper
+- Paper beats Rock
+- Rock beats Lizard
+- Lizard beats Spock
+- Spock beats Scissors
+- Scissors beats Lizard
+- Paper beats Spock
+- Rock beats Scissors
+- Lizard beats Paper
+- Spock beats Rock
+
+### Screenshot
+
+![](images/screenshot.png)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
--- app/js/script.js
+++ app/js/script.js
@@ -20,6 +20,7 @@ const createModal = () => {
   dialog.classList.add("rules")
   h2.innerText = "Rules"
   closeBtn.id = "close-btn"
+  closeBtn.setAttribute("aria-label", "Close")
   dialog.append(h2, closeBtn)
   document.body.appendChild(dialog)
   closeBtn.addEventListener("click", () => {
@@ -65,7 +66,7 @@ class Computer extends Player {
     super()
   }
 
-  pick() {
+  randomPick() {
     const randomPick = Math.floor(Math.random() * 5)
     const picked = super.options.find((el, id) => id === randomPick)
     return picked
@@ -75,25 +76,26 @@ class Computer extends Player {
 class User extends Player {
   _score = 0
 
-  constructor(btnPick, value = 0) {
+  constructor(btnPick, gameData = 0) {
     super()
-    this.score = { value, operation: "add" }
+    this.score = { initialLoad: true, operation: "add", value: gameData }
     this.btnPick = btnPick
     this.btnPick.addEventListener("click", (event) => this.pick(event))
+    updateScore(this.score)
   }
 
   pick(event) {
     if (event.target.matches(".pick")) {
       const choice = event.target.dataset.value
-      switch (decideWinner(choice)) {
+      const userPick = super.options.find((el) => el.type === choice)
+      switch (decideWinner(userPick)) {
         case "Win":
-          this.score = { value: 1, operation: "add" }
+          this.score = { initialLoad: false, operation: "add" }
           break
         case "Lose":
-          this.score = { value: 1, operation: "minus" }
+          this.score = { initialLoad: false, operation: "minus" }
           break
       }
-      console.log(this.score)
     }
   }
 
@@ -101,32 +103,36 @@ class User extends Player {
     return this._score
   }
 
-  set score({ value, operation }) {
+  set score({ initialLoad, operation, value = 1 }) {
     if (operation === "add") this._score = this.score + value
     else this._score = this.score ? this.score - value : 0
+    updateScore(this.score, initialLoad)
+    localStorage.setItem("RPSLS_Game_Score", this.score)
   }
 }
 
+const gameData = Number(localStorage.getItem("RPSLS_Game_Score")) ?? 0
 const btnPick = document.querySelector("main")
 const computer = new Computer()
-const user = new User(btnPick)
+const user = new User(btnPick, gameData)
 
 function decideWinner(userPick) {
-  const computerPick = computer.pick()
+  const computerPick = computer.randomPick()
   let status = "Draw"
-  console.log(userPick)
-  console.log(computerPick)
 
-  if (userPick !== computerPick.type) {
-    status = computerPick.beats.includes(userPick) ? "Lose" : "Win"
+  if (userPick.type !== computerPick.type) {
+    status = computerPick.beats.includes(userPick.type) ? "Lose" : "Win"
   }
 
-  showEndState({ status, computerPick: computerPick.type, userPick })
+  showEndState({
+    status,
+    computerPick: computerPick.type,
+    userPick: userPick.type,
+  })
   return status
 }
 
 function showEndState({ status, computerPick, userPick }) {
-  console.log(status)
   const playAgain = document.querySelectorAll(".play-again")
   const appDefaultState = document.querySelector(".start")
   const appEndState = document.querySelector(".end")
@@ -134,11 +140,15 @@ function showEndState({ status, computerPick, userPick }) {
   const computer = document.querySelector("#computer")
   const result = document.querySelectorAll(".result")
   const header = document.querySelector("header")
+  const mobileResult = document.querySelector(".end__blk2")
+  const desktopWidth = document.querySelector(".end")
+  const desktopResult = document.querySelector(".end__blk1--result")
 
   playAgain.forEach((element) => {
     element.addEventListener("click", () => {
       appDefaultState.classList.remove("hide")
       appEndState.classList.add("hide")
+      header.classList.remove("header__endState")
       user.setAttribute("class", "")
       computer.setAttribute("class", "")
     })
@@ -146,11 +156,38 @@ function showEndState({ status, computerPick, userPick }) {
 
   appDefaultState.classList.add("hide")
   appEndState.classList.remove("hide")
+  header.classList.add("header__endState")
   user.classList.add(`end__blk1--${userPick}`)
-  computer.classList.add(`end__blk1--${computerPick}`)
+  computer.classList.add(`end__blk1--picking`)
+  mobileResult.classList.add("transition")
+  desktopResult.classList.add("transition")
+  desktopWidth.classList.add("desktop-width")
+
+  setTimeout(() => {
+    computer.classList.remove(`end__blk1--picking`)
+    computer.classList.add(`end__blk1--${computerPick}`)
+  }, 700)
+
+  setTimeout(() => {
+    if (status !== "Draw") {
+      status === "Win"
+        ? user.classList.add("winner")
+        : computer.classList.add("winner")
+    }
+    mobileResult.classList.remove("transition")
+    desktopResult.classList.remove("transition")
+    desktopWidth.classList.remove("desktop-width")
+  }, 1100)
+
   result.forEach(
     (element) =>
       (element.innerText = status === "Draw" ? "Draw" : `You ${status}`)
   )
-  header.classList.add("header__endState")
+}
+
+function updateScore(score, initialLoad) {
+  const scoreBoard = document.querySelector("#score-board")
+  if (!initialLoad)
+    return setTimeout(() => (scoreBoard.innerText = score), 1800)
+  scoreBoard.innerText = score
 }
